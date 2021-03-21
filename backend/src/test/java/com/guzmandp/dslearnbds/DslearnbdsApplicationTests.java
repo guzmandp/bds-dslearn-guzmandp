49031d358decf533c7ab7cdd2ae593fe702b8d2e
Darwin Guzman <guzmandp@gmail.com>
2021-03-21 11:39:46 -0300

    Backend project created

---
+++ backend/src/test/java/com/guzmandp/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzmandp.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
